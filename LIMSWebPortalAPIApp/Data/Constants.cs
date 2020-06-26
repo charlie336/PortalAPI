5265b122e7c5ce8b9165a50d0006aa5d4e66c34b
charlie336 <45953920+charlie336@users.noreply.github.com>
2020-06-25 21:16:37 -0700

    no

---
+++ LIMSWebPortalAPIApp/Data/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LIMSWebPortalAPIApp.Data
+{
+    public static class Constants
+    {
+        public const string Audiance = "https://localhost:44394/";
+    }
+}
