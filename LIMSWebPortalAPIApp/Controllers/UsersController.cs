2fe41d7f44f97035a5d1d0fd992d513e24bdc5cd
charlie336 <45953920+charlie336@users.noreply.github.com>
2020-06-26 21:17:44 -0700

    day

--- LIMSWebPortalAPIApp/Controllers/UsersController.cs
+++ LIMSWebPortalAPIApp/Controllers/UsersController.cs
@@ -12,6 +12,7 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using System.Security.Claims;
 using System.IdentityModel.Tokens.Jwt;
+using LIMSWebPortalAPIApp.Contracts;
 
 namespace LIMSWebPortalAPIApp.Controllers
 {
@@ -22,27 +23,58 @@ namespace LIMSWebPortalAPIApp.Controllers
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
         private readonly IConfiguration _config;
-
+        private readonly ILoggerService _logger;
         public UsersController(SignInManager<IdentityUser> signInManager, 
             UserManager<IdentityUser> userManager,
-            IConfiguration config)
+            IConfiguration config,
+            ILoggerService logger)
         {
             _signInManager = signInManager;
             _userManager = userManager;
             _config = config;
+            _logger = logger;
+        }
+        [Route("Register")]
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] AppUserDTO userDTO)
+        {
+            var location = GetControllerActionNames();
+            try
+            {
+                var username = userDTO.Email;
+                var password = userDTO.Password;
+                _logger.LogInfo($"{location}: Registration attempt for {username}");
+                var user = new IdentityUser { Email = username, UserName = username };
+                var result = await _userManager.CreateAsync(user, password);
+                if (!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        _logger.LogError($"{location}: {error.Code} {error.Description}");
+                    }
+                    return InternalError($"{location}: {username} User Registration Attempt Failed");
+                }
+                return Ok(new { result.Succeeded });
+            }
+            catch(Exception e)
+            {
+                return InternalError($"location:{ e.Message} - {e.InnerException}");
+            }
+            
         }
 
         [AllowAnonymous]
         [HttpPost("Login")]
         public async Task<IActionResult> Login([FromBody] AppUserDTO userDTO)
         {
-            var username = userDTO.Username;
+            //var username = userDTO.Username;
+            var email = userDTO.Email;
             var password = userDTO.Password;
             var location = GetControllerActionNames();
-            var result = await _signInManager.PasswordSignInAsync(username, password, false, false);
+            var result = await _signInManager.PasswordSignInAsync(email, password, false, false);
             if(result.Succeeded)
             {
-                var user = await _userManager.FindByNameAsync(username);
+                var user = await _userManager.FindByNameAsync(email);
                 var tokenstring = await GenerateJSONToken(user);
                 return Ok(new {token = tokenstring });
             }
@@ -75,6 +107,12 @@ namespace LIMSWebPortalAPIApp.Controllers
             var controller = ControllerContext.ActionDescriptor.ControllerName;
             var action = ControllerContext.ActionDescriptor.ActionName;
             return $"{controller}-{action}";
+        }
+
+        private ObjectResult InternalError(string message)
+        {
+            _logger.LogError(message);
+            return StatusCode(500, "Something went wrong. Please contact the Administrator");
         }
     }
 }
--- LIMSWebPortalAPIApp/Data/DTOs/AppUserDTO.cs
+++ LIMSWebPortalAPIApp/Data/DTOs/AppUserDTO.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace LIMSWebPortalAPIApp.Data.DTOs
 {
     public class AppUserDTO
     {
-        public string Username { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        //public string Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(15, ErrorMessage = "Password is limited to {1} characters"), MinLength(6)]
         public string Password { get; set; }
     }
 }
